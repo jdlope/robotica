81d6be4dd8ca811aa5c80af909123c57230de660
Javier de Lope <jdlope@outlook.com>
2023-02-28 17:47:39 +0100

    Add module to communicate with CoppeliaSim (zmq)
    The module defines the communication between CoppeliaSim robotics
    simulator and external Python applications via the ZeroMQ remote API.
    It provides a class to handle the communication with the simulator and
    to manage the simulation and a class to control the Pioneer P3DX (motors
    and sonar).

---
+++ coppelia/zmqRemoteApi/python/robotica.py
@@ -0,0 +1,91 @@
+'''
+robotica.py
+
+Provides the communication between CoppeliaSim robotics simulator and
+external Python applications via the ZeroMQ remote API.
+
+Copyright (C) 2023 Javier de Lope
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import time
+
+from zmqRemoteApi import RemoteAPIClient
+
+
+class Coppelia():
+
+    def __init__(self):
+        print('*** connecting to coppeliasim')
+        client = RemoteAPIClient()
+        self.sim = client.getObject('sim')
+
+    def start_simulation(self):
+        # print('*** saving environment')
+        self.default_idle_fps = self.sim.getInt32Param(self.sim.intparam_idle_fps)
+        self.sim.setInt32Param(self.sim.intparam_idle_fps, 0)
+        self.sim.startSimulation()
+
+    def stop_simulation(self):
+        # print('*** stopping simulation')
+        self.sim.stopSimulation()
+        while self.sim.getSimulationState() != self.sim.simulation_stopped:
+            time.sleep(0.1)
+        # print('*** restoring environment')
+        self.sim.setInt32Param(self.sim.intparam_idle_fps, self.default_idle_fps)
+        print('*** done')
+
+    def is_running(self):
+        return self.sim.getSimulationState() != self.sim.simulation_stopped
+
+
+class P3DX():
+
+    num_sonar = 16
+    sonar_max = 1.0
+
+    def __init__(self, sim, robot_id):
+        self.sim = sim
+        print('*** getting handles', robot_id)
+        self.left_motor = self.sim.getObject(f'/{robot_id}/leftMotor')
+        self.right_motor = self.sim.getObject(f'/{robot_id}/rightMotor')
+        self.sonar = []
+        for i in range(self.num_sonar):
+            self.sonar.append(self.sim.getObject(f'/{robot_id}/ultrasonicSensor[{i}]'))
+
+    def get_sonar(self):
+        readings = []
+        for i in range(self.num_sonar):
+            res,dist,_,_,_ = self.sim.readProximitySensor(self.sonar[i])
+            readings.append(dist if res == 1 else self.sonar_max)
+        return readings
+
+    def set_speed(self, left_speed, right_speed):
+        self.sim.setJointTargetVelocity(self.left_motor, left_speed)
+        self.sim.setJointTargetVelocity(self.right_motor, right_speed)
+
+
+def main(args=None):
+    coppelia = Coppelia()
+    robot = P3DX(coppelia.sim, 'PioneerP3DX')
+    robot.set_speed(+1.2, -1.2)
+    coppelia.start_simulation()
+    while (t := coppelia.sim.getSimulationTime()) < 3:
+        print(f'Simulation time: {t:.3f} [s]')
+    coppelia.stop_simulation()
+
+
+if __name__ == '__main__':
+    main()
